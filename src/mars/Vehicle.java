e7a696d14704ea68494e42800a7b94e769671cef
Jekberg <john.berg.96bits@gmai.com>
2019-11-09 02:03:30 +0000

    Fixing the colab impl and adding a new act method

--- src/mars/Vehicle.java
+++ src/mars/Vehicle.java
@@ -2,6 +2,7 @@ package mars;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.stream.Stream;
 
 class Vehicle extends Entity{
 	public boolean carryingSample;
@@ -13,63 +14,60 @@ class Vehicle extends Entity{
 
 	public void act(Field f, Mothership m, ArrayList<Rock> rocksCollected)
 	{
-		actCollaborative(f, m, rocksCollected);
+		actAdvanced(f, m, rocksCollected);
+		//actCollaborative(f, m, rocksCollected);
 		//actSimple(f, m, rocksCollected);
 	}
 	
+	public void actAdvanced(Field f, Mothership m, ArrayList<Rock> rocksCollected){	
+		rule1DropSample(f);
+		
+		rule5DropTrail(f);
+		
+		rule3PickupSample(f, rocksCollected);
+		
+		rule7SearchForTrail(f);
+		
+		rule8FollowTrail(f);
+		
+		// Default action
+		rule4MoveRandomly(f);
+	}
 	
 	public void actCollaborative(Field f, Mothership m, ArrayList<Rock> rocksCollected){
-		// Rule 1
-		DropSample(f);
+		rule1DropSample(f);
 		
-		// Rule 5
-		DropTrail(f);
+		rule5DropTrail(f);
 		
-		// Rule 2
-		TravelUpGradient(f);
+		rule3PickupSample(f, rocksCollected);
 		
-		// Rule 6
-		FollowTrail(f);
+		rule6FollowTrail(f);
 		
-		// Rule 3
-		PickupSample(f, rocksCollected);
-		
-		// Rule 4
 		// Default action
-		moveRandomly(f);
+		rule4MoveRandomly(f);
 	}
 
 	public void actSimple(Field f, Mothership m, ArrayList<Rock> rocksCollected){
 		
-		// Rule 1
-		DropSample(f);
+		rule1DropSample(f);
 		
-		// Rule 2
-		TravelUpGradient(f);
+		rule2ReturnToShip(f);
 		
-		// Rule 3
-		PickupSample(f, rocksCollected);
+		rule3PickupSample(f, rocksCollected);
 		
-		// Rule 4
 		// Default action
-		moveRandomly(f);
+		rule4MoveRandomly(f);
 	}
 	
-	private void DropSample(Field f) {
+	private void rule1DropSample(Field f) {
 		if(carryingSample && f.isNeighbourTo(location, Mothership.class))
 			carryingSample = false;
 	}
 	
-	private void DropTrail(Field f) {
-		if(carryingSample && !f.isNeighbourTo(location, Mothership.class))
-			f.dropCrumbs(location, 1);
-	}
-	
-	private void TravelUpGradient(Field f) {
+	private void rule2ReturnToShip(Field f) {
 		if(carryingSample && !f.isNeighbourTo(location, Mothership.class))
 		{
 			var currentSignal = f.getSignalStrength(location);
-			
 			var newLoc = f.getAllfreeAdjacentLocations(location)
 				.stream()
 				.filter(loc -> currentSignal <= f.getSignalStrength(loc))
@@ -81,23 +79,7 @@ class Vehicle extends Entity{
 		}
 	}
 	
-	private void FollowTrail(Field f) {
-		if(f.getCrumbQuantityAt(location) != 0)
-		{
-			f.pickUpACrumb(location);
-			var currentSignal = f.getSignalStrength(location);
-			var newLoc = f.getAllfreeAdjacentLocations(location)
-				.stream()
-				.filter(loc -> currentSignal >= f.getSignalStrength(loc))
-				.min((l, r) -> f.getSignalStrength(l) - f.getSignalStrength(r))
-				.orElse(location);
-			f.clearLocation(location);
-			f.place(this, newLoc);
-			location = newLoc;
-		}
-	}
-	
-	private void PickupSample(Field f, ArrayList<Rock> rocksCollected) {
+	private void rule3PickupSample(Field f, ArrayList<Rock> rocksCollected) {
 		if (f.isNeighbourTo(location, Rock.class))
 		{
 			var rocLoc = f.getNeighbour(location, Rock.class);
@@ -107,7 +89,7 @@ class Vehicle extends Entity{
 		}
 	}
 	
-	private void moveRandomly(Field f) {
+	private void rule4MoveRandomly(Field f) {
 		
 		var newLocation = f.freeAdjacentLocation(location);
 		if(newLocation != null)
@@ -115,6 +97,89 @@ class Vehicle extends Entity{
 			f.clearLocation(location);
 			f.place(this, newLocation);
 			location = newLocation;
+		}
+	}
+	
+	private void rule5DropTrail(Field f) {
+		if(carryingSample && !f.isNeighbourTo(location, Mothership.class))
+		{
+			f.dropCrumbs(location, 2);
+			var currentSignal = f.getSignalStrength(location);
+			var newLoc = f.getAllfreeAdjacentLocations(location)
+				.stream()
+				.filter(loc -> currentSignal <= f.getSignalStrength(loc))
+				.max((l, r) -> f.getSignalStrength(l) - f.getSignalStrength(r))
+				.orElse(location);
+			f.clearLocation(location);
+			f.place(this, newLoc);
+			location = newLoc;
+		}
+	}
+	
+	private void rule6FollowTrail(Field f) {
+		if(f.getCrumbQuantityAt(location) != 0)
+		{
+			f.pickUpACrumb(location);
+			var currentSignal = f.getSignalStrength(location);			
+			var newLoc = f.getAllfreeAdjacentLocations(location)
+				.stream()
+				.filter(loc -> currentSignal >= f.getSignalStrength(loc))
+				.filter(loc -> f.getCrumbQuantityAt(loc) != 0)
+				.min((l, r) -> f.getSignalStrength(l) * f.getSignalStrength(r))
+				.orElse(location);
+			
+			if(newLoc == location)
+				newLoc = f.getAllfreeAdjacentLocations(location)
+				.stream()
+				.filter(loc -> currentSignal >= f.getSignalStrength(loc))
+				.min((l, r) -> f.getSignalStrength(l) * f.getSignalStrength(r))
+				.orElse(location);
+			
+			f.clearLocation(location);
+			f.place(this, newLoc);
+			location = newLoc;
+		}
+	}
+	
+	public void rule7SearchForTrail(Field f) {
+		if(!carryingSample && f.getCrumbQuantityAt(location) == 0)
+		{
+			var newLoc = f.getAllfreeAdjacentLocations(location)
+				.stream()
+				.filter(loc -> f.getCrumbQuantityAt(loc) != 0)
+				.findAny();
+			
+			if(newLoc.isPresent())
+			{
+				f.clearLocation(location);
+				f.place(this, newLoc.get());
+				location = newLoc.get();
+			}
+		}
+	}
+	
+	private void rule8FollowTrail(Field f) {
+		if(!carryingSample && f.getCrumbQuantityAt(location) != 0)
+		{
+			f.pickUpACrumb(location);
+			var currentSignal = f.getSignalStrength(location);			
+			var newLoc = f.getAllfreeAdjacentLocations(location)
+				.stream()
+				.filter(loc -> currentSignal >= f.getSignalStrength(loc))
+				.filter(loc -> f.getCrumbQuantityAt(loc) != 0)
+				.min((l, r) -> f.getSignalStrength(l) * f.getSignalStrength(r))
+				.orElse(location);
+			
+			if(newLoc == location)
+				newLoc = f.getAllfreeAdjacentLocations(location)
+				.stream()
+				.filter(loc -> currentSignal >= f.getSignalStrength(loc))
+				.min((l, r) -> f.getSignalStrength(l) * f.getSignalStrength(r))
+				.orElse(location);
+			
+			f.clearLocation(location);
+			f.place(this, newLoc);
+			location = newLoc;
 		}
 	}
 }
